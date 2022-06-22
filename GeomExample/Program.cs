9b208fc3beb3e3f4436d76d747f9414ba56f7dd4
Davide Maggiulli <davide.maggiulli@gmail.com>
2022-06-22 09:19:49 +0200

    Second commit

--- GeomExample/Program.cs
+++ GeomExample/Program.cs
@@ -18,7 +18,7 @@ namespace GeomExample
             a = new Point2D(100,13);
             b = new Point2D(-5, 19);
             
-            Point2D.Status = 20;
+            //Point2D.Status = 20;
 
             Console.WriteLine($"Punta A: ({a.GetX()},{a.GetY()})");
 
@@ -26,6 +26,7 @@ namespace GeomExample
             double dab = PointLib.GetDistance(a, b);
 
             Console.WriteLine($"Distanza tra due punti: {dab}");
+
 
             Point2D c = new Point2D(-5, 1);
             Point2D n1 = PointLib.GetNearestPointFromRef(a, b, c);
