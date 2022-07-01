4fc81f0c9d24b3bf87912a55f1673e2a783826d2
Davide Maggiulli <davide.maggiulli@gmail.com>
2022-07-01 14:34:38 +0200

    programmazione sincrona

---
+++ AsynchronousExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronousExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("** Asynchronous example **");
+            var watch = new Stopwatch();
+
+            watch.Start();
+
+            Task.WaitAll(Func1(), Func2(), Func3());
+            
+            watch.Stop();
+
+            Console.WriteLine($"End all activities: {watch.ElapsedMilliseconds}");
+        }
+
+        static async Task Func1()
+        {
+            Console.WriteLine("Func1 >> Start");
+            await Task.Delay(5000);
+            Console.WriteLine("Func1 >> End");
+        }
+
+        static async Task Func2()
+        {
+            Console.WriteLine("Func2 >> Start");
+            await Task.Delay(3000);
+            Console.WriteLine("Func2 >> End");
+        }
+
+        static async Task Func3()
+        {
+            Console.WriteLine("Func3 >> Start");
+            await Task.Delay(7000);
+            Console.WriteLine("Func3 >> End");
+        }
+    }
+}
